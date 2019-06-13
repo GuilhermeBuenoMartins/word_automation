83d58a0f96bf8f4ddd3135f5aad7a2d836c173d7
Bueno Martins <gbueno@indracompany.com>
2019-06-13 18:05:25 -0300

    first commit

---
+++ execute_word.py
@@ -0,0 +1,37 @@
+#execute_word.py
+
+#imports
+from log import Log
+from datetime import datetime
+from pywinauto.application import Application
+
+Log.open()
+
+#Here starting the automation
+Log.print('>>>The execution STARTS HERE!\n\n')
+dateExecution = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+app = Application(backend='uia')
+app.start('C:\Program Files\Microsoft Office\Office15\WINWORD.exe')
+word = app.Dialog
+
+#Creating a blank document
+word.child_window(title="Documento em branco", auto_id="AIOStartDocument", control_type="Hyperlink").type_keys('{ENTER}')
+
+#Typing and personalizing the document
+word.type_keys('Estou{SPACE}')
+word.child_window(title="Sublinhado", control_type="Button").click()
+word.child_window(title="Página 1", auto_id="UIA_AutomationId_Word_Page_1", control_type="Custom").type_keys('testando')
+word.child_window(title="Sublinhado", control_type="Button").click()
+word.child_window(title="Página 1", auto_id="UIA_AutomationId_Word_Page_1", control_type="Custom").type_keys('{SPACE}o{SPACE}Word.')
+
+#Saving the document
+word.child_window(title="Salvar", control_type="Button").click()
+word.child_window(title="Área de Trabalho", control_type="Button").click()
+
+#Encontrando a janela Salvar Como
+word.child_window(title="Nome do arquivo:", auto_id="1001", control_type="Edit").set_text(dateExecution)
+word.child_window(title="Salvar", auto_id="1", control_type="Button").click()
+Log.print('>>>The execution FINISHES HERE!\n\n')
+Log.close()
+exit()
---
+++ log.py
@@ -0,0 +1,57 @@
+#log.py
+
+'''
+It's a class provides a storer directory of logs.
+'''
+
+#Imports
+import logging
+import os
+from datetime import datetime
+from pywinauto import actionlogger
+
+
+class Log:
+	
+	def __init__(self):
+		self.logger = logging.getLogger(__name__)
+	
+	#Method of opening log.
+	def open():
+		try:
+			#Creating directory if it don't exist.
+			os.mkdir('.\Log')
+		except:
+			pass
+
+		#Setting up file log. 	
+		file = 'Log\\' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.log'
+		logging.basicConfig(
+		    level=logging.NOTSET,
+		    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+		    handlers=[
+		        logging.FileHandler(file),
+		        logging.StreamHandler()
+		    ])
+		#Starting a traceback registry for pywinauto
+		actionlogger.enable()
+
+
+
+	#Method of standard input into the log
+	def input(string):
+		value = input(string)
+		#self.logger.info(string + value)
+		return value
+
+	#Method of standard output into the log
+	def print(string):
+		logger.info(string)
+		print(string)
+
+	#Method of closing log.
+	def close():
+		#actionlogger.disable()
+		logger.close()
+
+
